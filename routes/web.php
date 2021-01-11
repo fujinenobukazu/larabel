1f27cbabdda2cae9405de4524be6c715b9589510
fujinenobukazu <nobu.volleyball@gmail.com>
2021-01-11 14:53:19 +0900

    タスク作成機能追加

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController; //これがないとコントローラーを探してくれない
 use App\Http\Controllers\FolderController; //これがないとコントローラーを探してくれない
 
-
+// トップページ
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
-
+// フォルダ作成機能
 Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
-
 Route::post('/folders/create', [FolderController::class, 'create']);
+// タスク作成機能
+Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
 
 // Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
 /*
