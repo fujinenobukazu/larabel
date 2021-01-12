9c2d7874300486b96f4b0dd7a41a3088159c265c
fujinenobukazu <nobu.volleyball@gmail.com>
2021-01-12 15:19:36 +0900

    トップページ作成　次にログイン機能実装（laravel8日推奨モデル）

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController; //これがないとコントローラーを探してくれない
 use App\Http\Controllers\FolderController; //これがないとコントローラーを探してくれない
+use App\Http\Controllers\HomeController; //これがないとコントローラーを探してくれない
 
-// トップページ
+
+// 管理ページ
 Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
 // フォルダ作成機能
 Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
@@ -15,19 +17,5 @@ Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
 // タスク編集機能
 Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
 Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
-
-// Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// トップページ
+Route::get('/', [HomeController::class, 'index'])->name('home');
