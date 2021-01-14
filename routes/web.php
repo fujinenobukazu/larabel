ee47bf185b52610bc81117f6671c793c5b721931
fujinenobukazu <nobu.volleyball@gmail.com>
2021-01-14 20:10:01 +0900

    認証機能 エラーハンドリング機能 追加

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,51 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController; //これがないとコントローラーを探してくれない
-use App\Http\Controllers\FolderController; //これがないとコントローラーを探してくれない
-use App\Http\Controllers\HomeController; //これがないとコントローラーを探してくれない
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\FolderController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('welcome', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+// ここから追記
+//
+Route::group(['middleware' => 'auth'], function() {
+
+  // トップページ
+  Route::get('/', [HomeController::class, 'index'])->name('home');
+  // フォルダ作成機能
+  Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
+  Route::post('/folders/create', [FolderController::class, 'create']);
 
 
-// 管理ページ
-Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
-// フォルダ作成機能
-Route::get('/folders/create', [FolderController::class, 'showCreateForm'])->name('folders.create');
-Route::post('/folders/create', [FolderController::class, 'create']);
-// タスク作成機能
-Route::get('/folders/{id}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
-Route::post('/folders/{id}/tasks/create', [TaskController::class, 'create']);
-// タスク編集機能
-Route::get('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
-Route::post('/folders/{id}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
-// トップページ
-Route::get('/', [HomeController::class, 'index'])->name('home');
+  Route::group(['middleware' => 'can:view,folder'], function() {
+    // 管理画面
+    Route::get('/folders/{folder}/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    // タスク作成機能
+    Route::get('/folders/{folder}/tasks/create', [TaskController::class, 'showCreateForm'])->name('tasks.create');
+    Route::post('/folders/{folder}/tasks/create', [TaskController::class, 'create']);
+    // フォルダ編集機能
+    Route::get('/folders/{folder}/tasks/{task_id}/edit', [TaskController::class, 'showEditForm'])->name('tasks.edit');
+    Route::post('/folders/{folder}/tasks/{task_id}/edit', [TaskController::class, 'edit']);
+  });
+
+});
